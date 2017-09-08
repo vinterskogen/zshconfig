d902154384a4569f8435ef0b14e653d24766ed0d
Vinter Skogen <vinter@vinterskogen.net>
2017-09-09 02:39:52 +0300

    Working on installer

--- install.sh
+++ install.sh
@@ -29,7 +29,7 @@ echo
 # Test current shell
 TEST_CURRENT_SHELL=$(expr "$SHELL" : '.*/\(.*\)')
 if [ "$TEST_CURRENT_SHELL" != "zsh" ]; then
-  echo "${RED}Make sure you are using ZSH shell!${NORMAL}"
+  echo "${YELLOW}Notice: make sure you are using ZSH!${NORMAL}"
   echo
 fi;
 
@@ -38,7 +38,7 @@ ZSH_CONFIG_DIR=$(dirname "$(readlink -f "$0")")
 
 # Check minimal requirements
 if ! [ -x "$(command -v git)" ]; then
-  echo "Error: git is not installed." >&2
+  echo "${RED}Error: git is not installed.${NORMAL}" >&2
   exit 1
 fi
 
@@ -51,23 +51,26 @@ echo -n "- Cheching for existing installation... "
 
 # Download Oh My Zsh
 if [ ! -e $OH_MY_ZSH_DIR ]; then
-  echo "${GREEN}not found!${NORMAL}"
+  echo "${BLUE}not found.${NORMAL}"
   echo -n "- Downloading Oh My Zsh... "
   git clone --depth=1 --quiet https://github.com/robbyrussell/oh-my-zsh.git "$OH_MY_ZSH_DIR"
+  if [ ! $? -eq 0 ] ; then
+    echo "${RED}failed!${NORMAL}"
+    exit 1
+  fi
   echo "${GREEN}done!${NORMAL}"
 else
-  echo "${BOLD}found.${NORMAL}"
+  echo "${BLUE}found.${NORMAL}"
   echo "  ${YELLOW}Notice: Oh My Zsh is already installed.${NORMAL}"
   echo "  ${YELLOW}(You'll need to remove $OH_MY_ZSH_DIR if you want to re-install it.)${NORMAL}"
-  echo
 fi
 
 # Moving old ZSH configuration files
 if [ -e $HOME/.zshrc ]; then
-  mv "$HOME/.zshrc" "$HOME/.oh-my-zsh/.zshrc.old"
+  mv "$HOME/.zshrc" "$HOME/.zshrc.old"
 fi
 if [ -e $HOME/.zsh_aliases ]; then
-  mv "$HOME/.zsh_aliases" "$HOME/.oh-my-zsh/.zsh_aliases.old"
+  mv "$HOME/.zsh_aliases" "$HOME/.zsh_aliases.old"
 fi
 
 # Linking ZSH configuration files
